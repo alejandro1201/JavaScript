938c739e1d440dd547bde7d9d78b1c65a597e6ec
Joan Alejandro Rodriguez Ramirez <102228998+alejandro1201@users.noreply.github.com>
2022-08-10 08:56:43 -0500

    Actualizacion
    12/08/2022

--- Pelotas/pelotas.js
+++ Pelotas/pelotas.js
@@ -1,56 +1,121 @@
 /**
  * Practica de POO usando canvas
- * 05/08/2022
+ * MLM
+ * 2022/08/05
+ */
+
+/** CARGAR RECURSOS */
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var pelotas = [];
+var cantidad = 50;
+var colores = [
+    'Red',
+    'Cyan',
+    'Blue',
+    'DarkBlue',
+    'LightBlue',
+    'Purple',
+    'Yellow',
+    'lime',
+    'Magenta',
+    'Pink',
+    'Silver',
+    'Black',
+    'Orange',
+    'Brown',
+    'Maroon',
+    'Green',
+    'Olive',
+    'Aquamarine',
+    'Red',
+    'Grey'    
+]
+
+
+/**
+ * DEFINICION DE LA CLASE 
+ */
+
+ class Pelota{
+    constructor(){
+        this.radio = generarNumero(20, 100);
+        this.centro = {x: generarNumero(this.radio, canvas.width - this.radio), 
+            y: generarNumero(this.radio, canvas.height - this.radio)};
+        this.vel = {x: generarNumero(-5, +5), y:generarNumero(-5, +5)};
+        this.colorBorde = colores[generarNumero(0, colores.length)];
+        this.colorRelleno = colores[generarNumero(0, colores.length)];
+    }
+
+    dibujar(){
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = this.colorBorde;
+        ctx.fillStyle=this.colorBorde;
+        ctx.arc(this.centro.x, this.centro.y, this.radio, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();  
+
+    }
+
+    mover(){
+        this.centro.x += this.vel.x;
+        this.centro.y += this.vel.y;
+        // Controlar rebotes 
+        if (this.centro.x + this.radio >= canvas.width || this.centro.x <= this.radio) {
+            this.vel.x *= -1;
+        }
+        if (this.centro.y + this.radio>= canvas.height || this.centro.y <= this.radio) {
+            this.vel.y *= -1;
+        }
+
+    }
+}
+
+
+
+
+/**
+ * FUNCIONES AUXILIARES
  * 
  */
 
-/**
- * CARGAR RECURSOS
- */
- var canvas = document.getElementById("canvas");
- var ctx = canvas.getContext("2d");
+function generarNumero(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
 
-/**
- * VALORES INICIALES
+/***************************************************************************
+ * PROGRAMA PRINCIPAL 
  */
-var centro = {x:100,y:100};
-var radio = 90;
-var vel = {x:5,y:2};
+
+
+
 animar();
-/**
- * DIBUJAR EL MUNDO
- */
+
+/** DIBUJAR EL MUNDO */
 function dibujar() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = 'red';
-    ctx.arc(centro.x, centro.y, radio, 0, 2 * Math.PI);
-    ctx.stroke();
+    for (let i = 0; i < cantidad; i++) {
+        pelotas.push(new Pelota());
+        pelotas[i].dibujar();
+    }
+
 }
-/**
- * CALCULAR VALORES
- */
+/** CALCULAR VALORES */
 function calcular() {
-    centro.x += vel.x;
-    centro.y += vel.y;
-    if (centro.x +radio>= canvas.width || centro.x <= radio ) {
-        vel.x *=-1;
-        
+    for (let i = 0; i < cantidad; i++) {
+        pelotas.push(new Pelota());
+        pelotas[i].mover();
     }
-   
-    if (centro.y +radio>= canvas.height || centro.y <= radio) {
-        vel.y *=-1;
-       
-    }
-    
+
+
 }
-/**
- * CICLO DE ANIMACION
- */
- 
+
+/** CICLO DE ANIMACION */
 function animar() {
     dibujar();
     calcular();
     requestAnimationFrame(animar);
 }
+
+/********************************************************************* */
